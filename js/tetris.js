a8518940c2194f855d87a380ec3caf1bad5dde8d
dldnjswns31 <dldnjswns32@gmail.com>
2021-10-28 04:57:56 +0900

    Make Matrix

--- js/tetris.js
+++ js/tetris.js
@@ -1,12 +1,13 @@
+// DOM
 const playground = document.querySelector('.playground > ul');
-console.log(playground);
 
 for(let i=0; i<20; i++){
-    const li = document.createElement('ul');
+    const li = document.createElement('li');
     const ul = document.createElement('ul');
     for(let j=0; j<10; j++){
         const matrix = document.createElement('li');
         ul.prepend(matrix);
     }
     li.prepend(ul);
+    playground.prepend(li);
 }
